3d12c5f63caf82057d0694d27b321df62476e044
Sagnik Ghosh <83448857+Sagnik-Coder24@users.noreply.github.com>
2021-09-11 20:34:14 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About Component.
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, doloremque cumque ducimus, velit similique culpa consectetur placeat maxime nesciunt doloribus, quia corrupti nobis impedit. Soluta ullam velit ad in non et sit quam vel.</p>
+        </div>
+    )
+}
